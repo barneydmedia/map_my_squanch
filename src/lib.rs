ceb17ebc5f777bef7d940639e788460193b61e1f
constructor <constructor@destructor.local>
2021-06-20 14:02:24 -0700

    add new methods

--- src/lib.rs
+++ src/lib.rs
@@ -9,47 +9,14 @@ mod map_option;
 mod block_options;
 mod map_collection;
 
-use noise::{Fbm, utils::NoiseMapBuilder, utils::*, OpenSimplex, Add, Seedable};
-use noise::utils::PlaneMapBuilder;
-use std::sync::{RwLock, Arc, Mutex};
-use rand::Rng;
-use rayon::prelude::*;
 use bencher::Bencher;
 
 use map_collection::{MapCollection2D, map2d::terrain_map2d::TerrainMap2D};
 use map_option::{MapOption};
 use block_options::{BlockOption};
 
-pub fn map_collection(x_size:usize, y_size:usize) -> MapCollection2D {
+pub fn new(x_size:usize, y_size:usize) -> MapCollection2D {
   return MapCollection2D::new(x_size, y_size);
-}
-
-fn map(options: MapOption) -> TerrainMap2D {
-  return TerrainMap2D::new(options.x_size(), options.y_size());
-}
-
-fn block(options: &BlockOption) -> TerrainMap2D {
-  let block_cell = Arc::new(Mutex::new(TerrainMap2D::new(options.x_size(), options.y_size())));
-
-  let open_simplex = OpenSimplex::new();
-  let rand = rand::thread_rng().gen();
-  let fbm = Fbm::new().set_seed(rand);
-  let noise = Add::new(&fbm, &open_simplex);
-  let render = PlaneMapBuilder::new(&noise)
-    .set_size(options.x_size(), options.y_size())
-    .build();
-  let (map_x, map_y) = render.size();
-  let map_size = (map_x * map_y) as i32;
-
-  (0..map_size).into_par_iter().for_each(|i| {
-    let x = i as usize % options.x_size();
-    let y = (i as usize - x)/options.x_size();
-    let val = (render.get_value(x,y) * 100 as f64).abs() as i32;
-    let mut block = block_cell.lock().unwrap();
-    block.set_value(x,y,val);
-  });
-
-  return block_cell.lock().unwrap().clone();
 }
 
 #[cfg(test)]
@@ -57,28 +24,8 @@ mod tests {
     use super::*;
 
     #[test]
-    fn test_block() {
-      let opt = &BlockOption::new(5000, 1000);
-      let block = block(opt).clone();
-      let values = block.values();
-      let (x_size, y_size) = block.size();
-
-      assert_eq!(x_size, 5000);
-      assert_eq!(y_size, 1000);
-      let mut img = image::RgbImage::new(x_size as u32, y_size as u32);
-
-      for (x, y, pixel) in img.enumerate_pixels_mut() {
-        let block_val = values[(x as usize + (y as usize * x_size))] as f32;
-        let r = (5. * block_val).abs().ceil() as u8;
-        let g = (2.2 * block_val).abs().ceil() as u8;
-        let b = (2.2 * block_val).abs().ceil() as u8;
-        *pixel = image::Rgb([r, g, b]);
-      }
-      img.save(format!("test_images/{}.png", uuid::Uuid::new_v4())).unwrap();
-    }
-
-    #[test]
-    fn test_map() {
-      let _ = map(MapOption::new(2500, 1000));
+    fn map_collection() {
+      let map = new(1, 1);
+      map.render();
     }
 }
--- src/map_collection.rs
+++ src/map_collection.rs
@@ -1,7 +1,9 @@
 pub mod map2d;
 
 use map2d::Map2D;
+use std::sync::{{RwLock, Arc}};
 
+#[derive()]
 pub struct MapCollection2D {
     size: usize,
     x_size: usize,
@@ -22,11 +24,23 @@ impl MapCollection2D {
             size: x_size * y_size,
             x_size: x_size,
             y_size: y_size,
-            map: map_vec
+            map: map_vec,
         }
     }
 
     pub fn size(&self) -> usize {
         self.size
+    }
+
+    pub fn x_size(&self) -> usize {
+        self.x_size
+    }
+
+    pub fn y_size(&self) -> usize {
+        self.y_size
+    }
+
+    pub fn render(&self) -> &Self {
+        &self
     }
 }
